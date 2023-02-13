ccbcfcb7aca058b6a2b62c1137ca8f7549a4a5c6
Marco Valle <marco_97@live.com.mx>
2023-02-12 21:33:29 -0600

    Pokedex y MongoDB init - Seccion 7

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '2',
+  });
+
+  await app.listen(3000);
+}
+bootstrap();

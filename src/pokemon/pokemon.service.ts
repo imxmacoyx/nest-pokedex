66421758358d84ec84b59eba8c53925881f10f79
Marco Valle <marco_97@live.com.mx>
2023-02-16 16:40:36 -0600

    Configuración de Joi para validaciones de configuracion de entorno y actualizacion de readme

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NotFoundException } from '@nestjs/common/exceptions';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
@@ -10,10 +11,15 @@ import { Pokemon } from './entities/pokemon.entity';
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit:  number;
+
   constructor(
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
-  ){}
+    private readonly configService: ConfigService,
+  ){
+    this.defaultLimit =  configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name =  createPokemonDto.name.toLocaleLowerCase();
@@ -28,7 +34,7 @@ export class PokemonService {
 
   findAll( paginationDto: PaginationDto ) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
